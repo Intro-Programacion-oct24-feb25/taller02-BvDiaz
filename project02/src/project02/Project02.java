9dcb7e6c705143b517b5b59f1589cf1c418c674a
Bryan Diaz <vicentepomas.2007@gmail.com>
2024-10-17 11:22:34 -0500

    terce commit, trabajando con Git y GitHubClassroom

---
+++ project02/src/project02/Project02.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project02;
+
+/**
+ *
+ * @author utpl
+ */
+public class Project02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ejemplo dos básico de Java\n" +
+"Versionado a través de Git\n" +
+"Manejado desde GitHub\n" +
+"Además se usar GitHubClassroom");
+    }
+    
+}
